8a6cd1bf6602d4a1aa38566611208439c2962832
tehlightbringer@hotmail.com <tehlightbringer@hotmail.com>
2017-05-16 10:44:31 +0200

    Change API endpoint to a gateway

--- js/serverFunctions.js
+++ js/serverFunctions.js
@@ -1,5 +1,7 @@
+const APP_ID = "591ab4b210360c000123da863c898f5020fd444d5fd6175590492756";
+
 const sendTextToServer = (text) => {
-  let url = "https://us-central1-chatbot-165909.cloudfunctions.net/helloWorld";
+  let url = "https://neytopia.cloud.tyk.io/chatbot/";
   const content = {
     text: text,
     id: WHENHUB_USER_ID
@@ -7,6 +9,7 @@ const sendTextToServer = (text) => {
   return fetch(url, {
     method: 'POST',
     headers: {
+      'Authorization': APP_ID,
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(content)
