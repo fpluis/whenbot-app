0e60c82fab210ec88b5f7ec459430a9f32ef4646
tehlightbringer@hotmail.com <tehlightbringer@hotmail.com>
2017-05-17 09:27:56 +0200

    Make API gateway connection work

--- js/serverFunctions.js
+++ js/serverFunctions.js
@@ -1,12 +1,15 @@
+const APP_ID = "591ab4b210360c000123da86ed4fc3a056d14b6d7f4677b3d6a977f6";
+const API_URL = "https://neytopia.cloud.tyk.io/chatbot/";
+
 const sendTextToServer = (text) => {
-  let url = "https://us-central1-chatbot-165909.cloudfunctions.net/helloWorld";
   const content = {
     text: text,
     id: WHENHUB_USER_ID
   };
-  return fetch(url, {
+  return fetch(API_URL, {
     method: 'POST',
     headers: {
+      'Authorization': APP_ID,
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(content)
